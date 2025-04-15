36514f1354e57d366cc37cede9e35950390fbca3
freekroelofs <freekjroelofs@gmail.com>
2025-04-15 15:47:37 +0200

    add FFT

--- src/svlbisim_griduv.py
+++ src/svlbisim_griduv.py
@@ -3,24 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numpy.lib.recfunctions as rec
 
-def griduv(obs, out, ncells, fov):
-    maxbl = np.max(np.sqrt(obs.data['u']**2+obs.data['v']**2))
-    
-    if ncells%2 != 0:
-        ncells += 1
-    cellsize_smearlimit = 1/fov
-    ncells_smearlimit = int(round(maxbl*2/cellsize_smearlimit, 0))
-    if ncells < ncells_smearlimit:
-        print('Grid cell size too large, setting to uv-smearing limit')
-        ncells = ncells_smearlimit
-    print('Gridding visibilities in %s x %s grid'%(ncells, ncells))
-
-    # Stokes I only: move everything to v > 0
-    for i in range(len(obs.data)):
-        if obs.data['v'][i] < 0:
-            obs.data['v'][i] *= -1
-            obs.data['u'][i] *= -1
-            obs.data['vis'][i] = np.conj(obs.data['vis'][i])
+def gen_visgrid(obs, ncells, maxbl):
     visgrid = np.zeros((ncells, ncells), dtype='complex')
     sigmagrid = np.zeros((ncells, ncells))
     nums = np.zeros((ncells, ncells))
@@ -44,13 +27,42 @@ def griduv(obs, out, ncells, fov):
 
     visgrid = visgrid/nums
 
-    obs_grid = obs.copy()
+    return visgrid, sigmagrid, nums
+
+
+def griduv(obs, out, ncells, fov):
+    maxbl = np.max(np.sqrt(obs.data['u']**2+obs.data['v']**2))
+
+    # Check cell size
+    if ncells%2 != 0:
+        ncells += 1
+    cellsize_smearlimit = 1/fov
+    ncells_smearlimit = int(round(maxbl*2/cellsize_smearlimit, 0))
+    if ncells < ncells_smearlimit:
+        print('Grid cell size too large, setting to uv-smearing limit')
+        ncells = ncells_smearlimit
+    print('Gridding visibilities in %s x %s grid'%(ncells, ncells))
+
+    # Pre-process observation
+    # Stokes I only: move everything to v > 0
+    obs_preproc = obs.copy()
+    for i in range(len(obs_preproc.data)):
+        if obs_preproc.data['v'][i] < 0:
+            obs_preproc.data['v'][i] *= -1
+            obs_preproc.data['u'][i] *= -1
+            obs_preproc.data['vis'][i] = np.conj(obs_preproc.data['vis'][i])
+    
+    # Grid visibilities        
+    visgrid, sigmagrid, nums = gen_visgrid(obs_preproc, ncells, maxbl)
+
+    # Make observation object
+    obs_grid = obs_preproc.copy()
     obs_grid.data = obs_grid.data[0]
 
     griddata = []
     ctr = 0
-    for ugrid in range(len(nums)):
-        for vgrid in range(len(nums[0])):
+    for ugrid in range(len(visgrid)):
+        for vgrid in range(len(visgrid[0])):
             griddata.append(np.array((ctr, nums[ugrid, vgrid], 'SAT1', 'SAT2', 0.0, 0.0,
                             ugrid*(maxbl*2/ncells) - maxbl, vgrid*(maxbl*2/ncells) - maxbl,
                             visgrid[ugrid,vgrid], 0.0, 0.0, 0.0, sigmagrid[ugrid,vgrid], 0.0, 0.0, sigmagrid[ugrid,vgrid]),
@@ -58,6 +70,8 @@ def griduv(obs, out, ncells, fov):
             ctr +=1
     obs_grid.data=rec.stack_arrays((obs_grid.data, griddata), asrecarray=True, usemask=False)
     obs_grid.data = np.delete(obs_grid.data, [0])
+
+    # Get rid of zeros and save
     todel = []
     for i in range(len(obs_grid.data)):
         if np.abs(obs_grid.data['vis'][i]) == 0.0:
@@ -67,6 +81,35 @@ def griduv(obs, out, ncells, fov):
 
     return obs_grid
 
+def calc_fft(obs, out, ncells):
+    maxbl = np.max(np.sqrt(obs.data['u']**2+obs.data['v']**2))
+    
+    # FFT image: need to add visibility conjugates before gridding
+    obs_preproc = obs.copy()
+    obs_conj = []
+    for i in range(len(obs.data)):
+        obs_conj.append(np.array((obs.data['time'][i], obs.data['tint'][i], 'SAT1', 'SAT2', 0.0, 0.0,
+                -obs.data['u'][i], -obs.data['v'][i],
+                np.conj(obs.data['vis'][i]), 0.0, 0.0, 0.0, obs.data['sigma'][i], 0.0, 0.0, -obs.data['sigma'][i]),
+                dtype=eh.DTPOL_STOKES))
+    obs_preproc.data=rec.stack_arrays((obs.data, obs_conj), asrecarray=True, usemask=False)
+    
+    visgrid, sigmagrid, nums = gen_visgrid(obs_preproc, ncells, maxbl)
+
+    # Make image and rotate/flip
+    fft=np.abs(np.fft.fftshift(np.fft.ifft2(visgrid)))
+    fov = 1 / (maxbl*2/ncells)
+    im = eh.image.make_square(obs_preproc, ncells, fov)
+    im.imvec = fft.flatten()
+    im = im.rotate(np.pi/2)
+    img = im.imvec.reshape((im.ydim, im.xdim))
+    img = np.flip(img,0)
+    img = img.reshape(im.ydim**2)
+    im.imvec = img
+    im.save_fits(out + '_fft.fits')
+
+    return im
+
 
 def main(params):
     obsfile = params['outdir'] + '/' + params['outtag'] + '.uvfits'
@@ -74,7 +117,8 @@ def main(params):
     out = params['outdir'] + '/' + params['outtag']
     fov = float(params['fov'])*eh.RADPERUAS
     ncells = int(params['ncells'])
-    griduv(obs, out, ncells, fov)
+    obs_grid = griduv(obs, out, ncells, fov)
+    im_fft = calc_fft(obs, out, ncells)
 
     return 0
 
